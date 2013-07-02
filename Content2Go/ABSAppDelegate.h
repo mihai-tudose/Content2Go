d5024ab74f3b9d8f36ac9dfa1e9c504c63341f60
Absolute <absolute@Absolute-Mihai.local>
2013-07-02 14:19:11 +0300

    Initial Commit

---
+++ Content2Go/ABSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABSAppDelegate.h
+//  Content2Go
+//
+//  Created by Absolute on 7/2/13.
+//  Copyright (c) 2013 Absolute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Content2Go/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Content2Go
+//
+//  Created by Absolute on 7/2/13.
+//  Copyright (c) 2013 Absolute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABSAppDelegate class]));
+    }
+}
